921e58e249f07d9d87563de64631cfc6546db9bb
ultraDOOMslaya <bemaakestad@gmail.com>
2014-11-13 01:29:08 -0600

    functionally working hybrid

--- hybrid.cc
+++ hybrid.cc
@@ -19,10 +19,11 @@ int slice;
 typedef struct process {
   int p_id;
   int burst;
+  int burst_time; // original burst for wait_time calculations
   int arrival;
   int priority;
-  int absolute;
-  int timeleft;
+  int absolute; //original priority
+  int timeleft; //running count of io
   bool upper;
   int io;
   int trigger_age_up;
@@ -77,7 +78,7 @@ process  findMax(queue<process>* run_queue) {
   process start_process;
   for(int i=99; i>0; --i) {
     if(run_queue[i].size() > 0) {
-      cout << run_queue[i].size() << '\n';
+      cout << "total run queue size is: " << run_queue[i].size() << '\n';
       start_process = run_queue[i].front();
       run_queue[i].pop();
       
@@ -213,16 +214,45 @@ static bool sortArrivals (const process &x, const process &y) {
 void organizeArrivals(vector<process> *chunk_to_arrive) {
   sort (chunk_to_arrive->begin(), chunk_to_arrive->end(), sortArrivals);
 }
-void promo(queue<process>* run_queue, int tick) {
-  for(int i=0; i<100; ++i) {
-    if( 
+
+int promoProcess(process *p) {
+  int pritz = p->priority;
+  pritz += 10;
+  if(p->upper) {
+    if(pritz > 99) {
+      pritz = 99;
+    }
+  } else {
+    if(pritz > 49) {
+      pritz = 49;
+    }
+  }
+  return pritz;
+}
+
+void promo(queue<process>* run_queue, int tick, int curr_priority) {
+  cout << "THOU SHALT BE PROMOTED!" << '\n';
+  process tmp;
+  for(int i=curr_priority; i>0; --i) {
+    if(run_queue[i].size() > 0) {
+      while(run_queue[i].front().trigger_age_up == tick and run_queue[i].size() > 0) {
+        cout << "stuck in promos?" << '\n';
+        tmp = run_queue[i].front();
+        run_queue[i].pop();
+        tmp.priority = promoProcess(&tmp);
+        run_queue[tmp.priority].push(tmp);
+      }
+    }
   }
 }
 
-void sleuthIO(vector<process> wait_queue, queue<process>* run_queue ) {
-  int count;
-  for(vector<process>::iterator i=wait_queue.begin(); i!=wait_queue.end(); ++i) {
+/*void sleuthIO(vector<process> *wait_queue, queue<process>* run_queue ) {
+  int count=0;
+  //cout << "wait queue length is " << wait_queue.size() << '\n';
+  for(vector<process>::iterator i=wait_queue->begin(); i!=wait_queue->end(); ++i) {
+    cout << "sleuth?" << '\n';
     if(i->timeleft == 0) {
+      cout << "----- from the sleuthing: " << i->timeleft << '\n'; 
       i->priority += i->io;
       if(i->upper) {
         if(i->priority > 99) {
@@ -233,20 +263,57 @@ void sleuthIO(vector<process> wait_queue, queue<process>* run_queue ) {
           i->priority = 49;
         }
       }
+      cout << "do you die here?" << '\n';
       run_queue->push(*i);
-      wait_queue.erase(wait_queue.begin()+count);
+      cout << "how bout hur?" << '\n';
+      i->timeleft = -1; //send it to oblivion 
     } else {
-      --i->timeleft;
+      wait_queue->at(count).timeleft =  i->timeleft - 1;
+      cout << "IO: " << i->timeleft << " on " << i->p_id << "'s I/O." << '\n';
     }
     count++;
   }
+}*/
+
+void sleuthIO(queue<process>* wait_queue, queue<process>* run_queue) {
+  int count=0;
+  while(count < wait_queue->size()) {
+  //cout << "wait queue size: " << wait_queue->size() << " to count: " << count << '\n';
+  process tmp;
+  tmp = wait_queue->front();
+  wait_queue->pop();
+  if(tmp.timeleft == 0) {
+      cout << "----- from the sleuthing: " << tmp.timeleft << '\n'; 
+      tmp.priority += tmp.io;
+      if(tmp.upper) {
+        if(tmp.priority > 99) {
+          tmp.priority = 99;
+        }
+      } else {
+        if(tmp.priority > 49) {
+          tmp.priority = 49;
+        }
+      }
+      cout << "do you die here?" << '\n';
+      run_queue[tmp.priority].push(tmp);
+    } else {
+      tmp.timeleft =  tmp.timeleft - 1;
+      wait_queue->push(tmp);
+      //cout << "IO: " << i->timeleft << " on " << i->p_id << "'s I/O." << '\n';
+    }
+    ++count; 
+  }  
 }
 
 void demote(process being_processed, queue<process>* run_queue) {
+  cout << "_______________" << '\n';
+  cout << being_processed.p_id << " was demoted." << '\n';
+  cout << "---------------" << '\n';
   being_processed.priority -= slice;
   if(being_processed.priority < being_processed.absolute) {
     being_processed.priority = being_processed.absolute; 
   }
+  being_processed.trigger_age_up += 100;
   run_queue[being_processed.priority].push(being_processed);   
 }
 
@@ -263,15 +330,22 @@ int main() {
   int tick=0;
   process being_processed; // a tmp process we use in multiple spots
   vector<process> arrivals;
-  vector<process> wait_queue;
+  //vector<process> wait_queue;
+  queue<process> wait_queue;
   //vector<queue<process> > run_queue;
   queue<process> run_queue[100];
   int process_count=0;
   struct node *rr_tree;
   struct node *curr_node;
   struct root *head;
+  int not_processing=0;
+  int curr_priority=0;
+  int wait_time=0;
+  int avg_wait_time=0;
+  int total_process;
+  int turnaround=0;
 
-  fstream myfile ("10_processes"); 
+  fstream myfile ("100k_processes"); 
   int count=0;
   string buf;
   string line;
@@ -292,19 +366,47 @@ int main() {
       if(c > 1) {
         being_processed.p_id=atoi(tokens[0].c_str());
         being_processed.burst=atoi(tokens[1].c_str());
+        being_processed.burst_time=atoi(tokens[1].c_str());
         being_processed.arrival=atoi(tokens[2].c_str());
         being_processed.priority=atoi(tokens[3].c_str());
         being_processed.io=atoi(tokens[5].c_str()); 
         being_processed.absolute=atoi(tokens[3].c_str());
         being_processed.trigger_age_up=0;
         ++process_count;
-        arrivals.push_back(being_processed);
+        cout << being_processed.p_id << " ";
+        
       }
+      if(being_processed.p_id<0){
+        being_processed.p_id *= -1;
+        cout << being_processed.p_id << '\n';
+      }
+      if(being_processed.burst<0){
+        being_processed.burst *= -1;
+        cout << being_processed.p_id << '\n';
+      }
+      if(being_processed.burst_time<0){
+        being_processed.burst_time *= -1;
+        cout << being_processed.p_id << '\n';
+      }
+      if(being_processed.arrival<0){
+        being_processed.arrival *= -1;
+      }
+      if(being_processed.priority<0){
+        being_processed.priority *= -1;
+      }
+      if(being_processed.io<0){
+        being_processed.priority *= -1;
+      }
+      if(being_processed.absolute<0){
+        being_processed.priority *= -1;
+      }
+      arrivals.push_back(being_processed);
+      cout << being_processed.p_id << " was added" << '\n';
     }
   } else cout << "Unable to open file"; 
+  cout << '\n';
   
-  ageing_time = 100;
-   
+  total_process = process_count;
   /*cout << "processes: " << '\n';
   */
   //array to hold our ready queues
@@ -315,18 +417,19 @@ int main() {
   process p3 = {4, 200, 15, 3, 200, 0};
   process p4 = {5, 130, 15, 8, 130, 0};
   
-  vector<process> arrivals(5);
+  arrivals.resize(5);
   arrivals[0] = p0;
   arrivals[1] = p1;
   arrivals[2] = p2;
   arrivals[3] = p3;
   arrivals[4] = p4;
-  int process_count = 5;*/
+  process_count = 5;*/
   sort (arrivals.begin(), arrivals.end(), myfunction);
   vector<process>::iterator iter=arrivals.begin();
   vector<process> chunk_to_arrive;
   for(vector<process>::iterator i=arrivals.begin(); i<arrivals.end(); ++i) {
     cout << i->p_id << '\n';
+    cout << "and io " << i->io << '\n';
   }
   
   //make 100 nodes with 100 queues
@@ -335,7 +438,7 @@ int main() {
     cout << "inserted: " << num_nodes << '\n';
     cout << "head is: " << rr_tree->key << '\n';
   }*/
-      
+  //process_count = process_count-2;    
   while(process_count != 0) {
     //cout << "amount of processes to process left: " << process_count << '\n';
     if(arrivals.size() != 0) {
@@ -353,71 +456,105 @@ int main() {
     //cout << "about to pend" << '\n';
     if(pending_arrival) {
       for(vector<process>::iterator it=chunk_to_arrive.begin(); it!=chunk_to_arrive.end(); ++it) {
-        cout << "get here1" << '\n';
+        //cout << "get here1" << '\n';
         if(it->absolute > 49) {
           it->upper = true;
         }
         run_queue[it->priority].push(*it);
-        cout << "key for item just added to queue at leaf node: " << '\n';
+        //cout << "key for item just added to queue at leaf node: " << '\n';
         //cout << rr_tree->rr.front().p_id << '\n';
       } 
-      cout << "clock tick when we push to the queue: " << tick << '\n';
-      cout << "_________________" << '\n';
+      //cout << "clock tick when we push to the queue: " << tick << '\n';
+      //cout << "_________________" << '\n';
       chunk_to_arrive.clear();
     }
       
     //if tree isn't empty start processing
     if(pending_arrival) {
       if(!processing and !queued) {
-        cout << "about to set a process" << '\n';
+        //cout << "about to set a process" << '\n';
         //find the highest priority with a size > 0  
-        cout << "get b" << '\n';
+        //cout << "get b" << '\n';
         being_processed = findMax(run_queue);
+        curr_priority = being_processed.priority;
+        not_processing = -8;
         processing = true;
-        cout << being_processed.p_id << '\n'; 
+        processor_slice = 0;
+        //cout << being_processed.p_id << '\n'; 
       }
     } else if(!processing and queued) {
-        
+        cout << "logic error?" << '\n';
+       if(run_queue->size() > 0) {
+           processing = true;
+           being_processed = findMax(run_queue);
+           not_processing = -8;
+           processing = true;
+           processor_slice = 0;
+       } 
+       queued = false;
+    } else if(!processing and (run_queue->size() > 0)) {
+        being_processed = findMax(run_queue);
+        curr_priority = being_processed.priority;
+        not_processing = -8;
+        processing = true;
+        processor_slice = 0; 
     }
     
-      
       //
     if(processing) {
+      //cout << "being processed: " << being_processed.p_id << " with burst of... " << being_processed.burst << " at tick: " << tick << '\n'; 
       if(processor_slice == slice) {
         // check for clock interrupt
         if(being_processed.burst > 0) {
           demote(being_processed, run_queue);
+          processing = false;
         }
       } else if(processor_slice == (slice-1)) {
         // check for io
         if(being_processed.io > 0) {
           being_processed.timeleft = being_processed.io;
-          wait_queue.push_back(being_processed);
+          wait_queue.push(being_processed);
+          queued = true;
           processing = false;
         }  
       } else {
         --being_processed.burst;
-        //continue processing
+        //continue processing or finish when burst is 0
         if(being_processed.burst <= 0) {
           processing = false;
           --process_count;
-        } else {
-          being_processed.burst -= 1;
+          if(being_processed.p_id > 0) {
+            wait_time = wait_time + ((tick - being_processed.arrival) - being_processed.burst_time);
+            
+            turnaround = turnaround + (tick - being_processed.arrival);
+          }
+          cout << "being processed: " << being_processed.p_id << " finished at clock tick: " << tick << '\n'; 
+          cout << "processes left are: " << process_count << '\n';
         }
       }
-      sleuthIO(wait_queue, run_queue);
+      ++processor_slice;
     }
-    
-    promo(run_queue);
-  
+
+    if(wait_queue.size() > 0) {
+      //cout << "wakw waka" << '\n';
+      //cout << "wait queue size? " << wait_queue.size() << '\n';
+      sleuthIO(&wait_queue, run_queue);
+    }
+
+    //cout << "still looping?" << '\n';
+    if(not_processing < 99) {
+      promo(run_queue, tick, curr_priority);
+    }
+    /*if(not_processing > 5000) {
+      cout << "we are stuck and need help..." << '\n';
+      cout << "--------------" << '\n';
+      cout << "size of the run queue... " << run_queue->size() << '\n';
+      cout << "size of the wait queue... " << wait_queue.size() << '\n';
+    }*/
+    ++not_processing;
     ++tick;
-    if(tick == 8000) {
-      break;
-    }
-  }/*
+  }
   cout << "We done foo, total ticks are: " << tick << '\n';
-  cout << head->top->key << '\n';
-  cout << rr_queue->left->key << " " << rr_queue->right->key << '\n';
-  cout  << " " << rr_queue->right->left->key << " " << rr_queue->right->right->key << '\n';
-  cout << "       " << " " << rr_queue->right->right->left->key << '\n';*/ 
+  cout << "Average wait time is:       " << (wait_time/total_process) << '\n';
+  cout << "Average turnaround time is: " << (turnaround/total_process) << '\n';
 }
